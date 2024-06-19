e36f92bcba51a13eadaf1d4615fa06c4b466605b
RAKOTOARISOA Tahiriniaina Andrianina <ta.rakotoarisoa@soprasteria.com>
2024-06-19 10:34:49 +0200

    first commit

---
+++ src/main/java/com/project/we_go_jim/WeGoJimApplication.java
@@ -0,0 +1,13 @@
+package com.project.we_go_jim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeGoJimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeGoJimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/we_go_jim/WeGoJimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.we_go_jim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeGoJimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
