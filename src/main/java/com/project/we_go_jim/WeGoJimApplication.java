53a7c91e50d82eda1fe745293602be3368fad324
RAKOTOARISOA Tahiriniaina Andrianina <ta.rakotoarisoa@soprasteria.com>
2024-08-02 18:40:40 +0200

    feat(authentication): Update jobs and variables initialization.
    - Update workflow to create and initialize variables from repository variable.
    - Add configs to load variables for email and database.
    - Update application.yml.

--- src/main/java/com/project/we_go_jim/WeGoJimApplication.java
+++ src/main/java/com/project/we_go_jim/WeGoJimApplication.java
@@ -1,5 +1,6 @@
 package com.project.we_go_jim;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WeGoJimApplication {
 
     public static void main(String[] args) {
+        setUpdDBConnection();
         SpringApplication.run(WeGoJimApplication.class, args);
     }
 
+    /**
+     * Initialize database url, username and password from
+     * environment variable.
+     */
+    public static void setUpdDBConnection() {
+        Dotenv dotenv = Dotenv.load();
+        final String datasource_url = dotenv.get("DATASOURCE_URL");
+        final String datasource_username = dotenv.get("DATASOURCE_USERNAME");
+        final String datasource_password = dotenv.get("DATASOURCE_PASSWORD");
+
+        System.setProperty("datasource_url", datasource_url);
+        System.setProperty("datasource_username", datasource_username);
+        System.setProperty("datasource_password", datasource_password);
+    }
 }
--- src/main/java/com/project/we_go_jim/config/RedisConfig.java
+++ src/main/java/com/project/we_go_jim/config/RedisConfig.java
@@ -1,5 +1,6 @@
 package com.project.we_go_jim.config;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -11,9 +12,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
 
+    private final Dotenv dotenv = Dotenv.load();
+    private final String REDIS_HOST = dotenv.get("REDIS_HOST");
+    private final String REDIS_PORT = dotenv.get("REDIS_PORT");
+
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+        return new LettuceConnectionFactory(REDIS_HOST, Integer.parseInt(REDIS_PORT));
     }
 
     @Bean
